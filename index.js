78f39dddec2094d43c168011bb79a3a99c0de419
bluuweb <contacto@bluuweb.cl>
2024-04-19 21:37:35 -0400

    bootstrap npm

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
+import path from 'path'
 
 const app = express()
 
+// ruta absoluta
+const __dirname = import.meta.dirname
+
+// middleware
+app.use(express.static('public'))
+app.use('/assets/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/assets/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', './views');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
     res.render('home', { title: "Home Page 2.0", user: null })
@@ -15,8 +24,17 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
+const products = [
+    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
+    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
+    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
+    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
+]
+
+const fruts = ['🍉', '🍓', '🍌']
+
 app.get('/products', (req, res) => {
-    res.render('products')
+    res.render('products', { products: products, fruts })
 })
 
 const PORT = process.env.PORT || 3000
---
+++ public/assets/js/app.js
@@ -0,0 +1,1 @@
+console.log('todo andando oksss')
