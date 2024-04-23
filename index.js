7fd0408c8e71510f325576fe21df10fbbc284e54
pablofsj <saintjourpablo@gmail.com>
2024-04-22 21:29:40 -0400

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
-import express from 'express'
-import { engine } from 'express-handlebars';
-import path from 'path'
+import express from "express";
+import { engine } from "express-handlebars";
+import path from "path";
+import Jimp from "jimp";
+import { v4 as uuidv4 } from "uuid";
 
-const app = express()
+const app = express();
 
 // ruta absoluta
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
 // middleware archivos estáticos
-app.use(express.static('public'))
-app.use('/assets/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-app.use('/assets/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use(express.static("public"));
+app.use(
+  "/assets/css",
+  express.static(__dirname + "/node_modules/bootstrap/dist/css")
+);
+app.use(
+  "/assets/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
 
-app.engine('.hbs', engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', __dirname + '/views');
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", __dirname + "/views");
 
-app.get('/', (req, res) => {
-    res.render('home', { title: "Home Page 2.0", user: null })
-})
+app.get("/", (req, res) => {
+  res.render("home", { title: "Convertidor de imagenes a blanco y negro" });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.get("/image", async (req, res) => {
 
-const products = [
-    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
-    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
-    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
-    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
-]
+  try {
+    const urlImage = req.query.urlImage;
+    const image = await Jimp.read(urlImage);
+    const buffer = await image
+      .resize(350, Jimp.AUTO)
+      .grayscale()
+      .getBufferAsync(Jimp.MIME_JPEG);
 
-const fruts = ['🍉', '🍓', '🍌']
+    const dirnameImg = __dirname + `/public/assets/img/image-${uuidv4()}.jpeg`;
+    await image.writeAsync(dirnameImg);
+    res.set("Content-Type", "image/jpeg");
+    return res.send(buffer);
 
-app.get('/products', (req, res) => {
-    res.render('products', { products: products, fruts })
-})
+  } catch (error) {
+    const msgError = `Error en la solicitud de tipo: ${error.message}`;
+    console.error(error);
+    return res.status(500).send(msgError);
+  }
+});
 
-const PORT = process.env.PORT || 3000
+
+
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Example app listening on PORT ${PORT}`)
-})
+  console.log(`Example app listening on PORT ${PORT}`);
+});
